2ba729e67b3668297add14f3a29338e71ec71f03
dheerajanumala <dheeraj@verosai.com>
2025-06-11 11:22:48 -0400

    backend repo

---
+++ PulseTrackBackend/src/test/java/com/spring/pulsetrackbackend/PulseTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.pulsetrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseTrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
